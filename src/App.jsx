eec28c4bed7d72c857acd297b0eab5e4832c42ae
Marcela Rendon <marcis23@gmail.com>
2024-11-04 23:08:58 -0500

    Estructura Parcial Nov. 4, first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
